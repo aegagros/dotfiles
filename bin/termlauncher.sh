f816bc68ae46a30113c1b3ff64d0aeeefcfe9a82
Manolis Paterakis <manolis@opendesignstudio.gr>
2021-11-20 23:48:07 +0200

    Provide font and window size as arguments to termlauncher script

--- bin/termlauncher.sh
+++ bin/termlauncher.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-WINIT_X11_SCALE_FACTOR=1 alacritty \
-    --class termlauncher,termlauncher \
-    --option "window.dimensions.columns=180" \
-    --option "window.dimensions.lines=20" \
-    --option font.size=9.5 \
-    -e $1
+EXEC="WINIT_X11_SCALE_FACTOR=1 alacritty --class termlauncher,termlauncher"
 
+if [ $# -gt 1 ]
+then
+    FONTSIZE=$1
+    COLS=$2
+    ROWS=$3
+    CMD=$4
+    EXEC="$EXEC --option 'window.dimensions.columns=$COLS' --option 'window.dimensions.lines=$ROWS' --option font.size=$FONTSIZE -e $CMD"
+else
+    EXEC="$EXEC -e $1"
+fi
+
+#WINIT_X11_SCALE_FACTOR=1 alacritty \
+#    --class termlauncher,termlauncher \
+#    --option "window.dimensions.columns=180" \
+#    --option "window.dimensions.lines=20" \
+#    --option font.size="$FONTSIZE" \
+#    -e $CMD
+
+eval $EXEC
