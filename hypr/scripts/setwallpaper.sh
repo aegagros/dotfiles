c31f9687162d6e6ea88d2afae2ae0b76c7f89fba
Manolis Paterakis <manolispaterakis@gmail.com>
2025-01-06 22:51:35 +0200

    Fix theme selection

--- hypr/scripts/setwallpaper.sh
+++ hypr/scripts/setwallpaper.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
 
-CONTRAST="10.0"
-SATURATION="0.65"
+CONTRAST="8.0"
+SATURATION="0.5"
 IMGPATH=$1
+LIGHT="-l"
+BACKEND="colorz"
+
+function reload_spicetify_theme() {
+    local curr_theme="$(spicetify config | grep "^current_theme" | awk '{ print $2; }')"
+    if [[ -z "${curr_theme}" ]]; then
+        return
+    fi
+    echo "Current spicetify theme is: ${curr_theme}"
+    local colorscheme_path="${HOME}/.config/spicetify/Themes/${curr_theme}/color.ini"
+    local linenum=$(cat "${colorscheme_path}" | grep -nsh '^\[pywal\]' | awk -F':' '{ print $1; }')
+    echo "Existing pywal colorscheme definition found at line: ${linenum}"
+    if [[ ! -z "${linenum}" ]]; then
+        sed -i "${linenum},$ d" "${colorscheme_path}"
+    fi
+    echo -e "\n\n[pywal]" >> ${colorscheme_path}
+    cat ~/.cache/wal/colors-spicetify.ini >> ${colorscheme_path}
+    spicetify apply 
+}
+
+ln -sf "${IMGPATH}" ~/.wallpaper
 
 wal --cols16 \
+    ${LIGHT} \
+    --backend ${BACKEND} \
     --saturate ${SATURATION} \
     --contrast ${CONTRAST} \
     -n -i "${IMGPATH}"
 
 swww img "${IMGPATH}"
+# reload services
 pkill -SIGUSR2 waybar
+dunstctl reload
+reload_spicetify_theme
+
